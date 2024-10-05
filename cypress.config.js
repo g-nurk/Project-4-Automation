f844f78b7ec946f0bdf9f5cc7b5b5380661f0f69
g-nurk <gatlin.nurk@gmail.com>
2024-10-05 17:54:00 +0300

    My comments about the commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'g8ah8f',
     pageLoadTimeout: 15000,
 
     env: {
--- cypress/e2e/registration_form_3_test.cy.js
+++ cypress/e2e/registration_form_3_test.cy.js
@@ -14,7 +14,55 @@ Task list:
     * checkboxes, their content and links
     * email format
  */
+describe('Section 1: Visual tests', () => {
+    it('Check that Cerebrum Hub logo is correct and has correct size', () => {
+        cy.log('Will check Cerebrum Hub logo source and size')
+        cy.get('img').should('have.attr', 'src').should('include', 'cerebrum_hub_logo')
+        cy.get('img').invoke('height').should('be.lessThan', 178)
+            .and('be.greaterThan', 120)   
+        cy.get('img').invoke('width').should('be.lessThan', 180)
+            .and('be.greaterThan', 175)
+    })
+    it('Check that radio button list is correct', () => {
+    cy.get('input[type="radio"]').should('have.length', 4)
+    cy.get('input[type="radio"]').next().eq(0).should('have.text','Daily')
+    cy.get('input[type="radio"]').next().eq(1).should('have.text','Weekly')
+    cy.get('input[type="radio"]').next().eq(2).should('have.text','Monthly')
+    cy.get('input[type="radio"]').next().eq(3).should('have.text','Never')
+    cy.get('input[type="radio"]').eq(0).should('not.be.checked')
+    cy.get('input[type="radio"]').eq(1).should('not.be.checked')
+    cy.get('input[type="radio"]').eq(2).should('not.be.checked')
+    cy.get('input[type="radio"]').eq(3).should('not.be.checked')
+    cy.get('input[type="radio"]').eq(0).check().should('be.checked')
+    cy.get('input[type="radio"]').eq(1).check().should('be.checked')
+    cy.get('input[type="radio"]').eq(0).should('not.be.checked')
+    })
 
+    it('Check the dropdown and dependencies', () => {
+    cy.get('#country').children().should('have.length', 4)
+    cy.get('#country').find('option').eq(0).should('have.text', '')
+    cy.get('#country').find('option').eq(1).should('have.text', 'Spain')
+    cy.get('#country').find('option').eq(2).should('have.text', 'Estonia')
+    cy.get('#country').find('option').eq(3).should('have.text', 'Austria')
+    cy.get('#country').select('Spain')
+    cy.get('#city').select('Madrid')
+    //cy.get('#country').select('Estonia')
+    //cy.get('#city').select('Haapsalu')
+    cy.get('body').click()
+    })      
+
+    //ei oska 
+    it('Check that checbox list is correct', () => {
+    })
+
+    //ei oska
+    it('Check that the email format is correct', () => {
+        //cy.get('input[name="email"]').should('contain', '[a-z0-9]+@[a-z0-9]+\\.[a-z]{2,4}$')
+        cy.get('input[name="email"]').type('invalid')
+        //cy.get('#input_error_message').should('be.visible').should('contain', 'Invalid email address.')
+        //cy.get('ng-show="myForm.email.$error.email').should('be.visible').should('contain', 'Invalid email address.')
+    })
+})
 
 /*
 BONUS TASK: add functional tests for registration form 3
